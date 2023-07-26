0fffefb105ff108c76a171df12236d291ec043ec
Philip Simpson <dev.phits@gmail.com>
2023-07-25 18:56:45 -0700

    Update phone number

--- src/components/Utilities/Footer/Footer.jsx
+++ src/components/Utilities/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
       <div className='md:w-3/4 mx-auto min-h-[300px] flex flex-col justify-center items-center py-3 bg-black text-white gap-4 font-bold'>
         <h3 className=''>CONTACT US</h3>
-        <h3 className='underline underline-offset-8'>702.521.6001</h3>
+        <h3 className='underline underline-offset-8'>702.521.4063</h3>
         {/* <button
         type='button'
         className='border border-none rounded-sm px-3 py-2 bg-blue-700 text-sm hover:bg-blue-800'
