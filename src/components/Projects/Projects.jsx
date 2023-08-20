6e156c6302517a856e71c3fb6f93c4657e5bc83c
Philip Simpson <dev.phits@gmail.com>
2023-08-19 18:38:19 -0700

    Projects paragraph

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -20,11 +20,8 @@ const Projects = () => {
     <section className='bg-white w-full md:w-3/4 mx-auto my-0'>
       <div className='text-black flex flex-col items-center py-8 gap-4'>
         <h1 className=' font-extrabold'>OUR PROJECTS</h1>
-        <p className=' w-full md:w-3/4 mx-4 md:mx-0 px-4 md:px-0'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          eveniet enim commodi, iusto nulla molestiae odit consequatur quod
-          esse, magni laborum quasi! Dolores voluptas quaerat neque eos deleniti
-          omnis enim!
+        <p className='text-center w-full md:w-3/4 mx-4 md:mx-0 px-4 md:px-0 uppercase'>
+          Efficient. Clean. Durable.
         </p>
       </div>
       {showRes && <ProjectsResidential onClick={toggleProjects} />}
